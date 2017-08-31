4b1c4ef67382da3c434e241614fe222583c949a8
rodrigocipriani <chopsss@gmail.com>
2017-08-30 22:11:56 -0300

    Included Typography component

---
+++ src/style/Typography/Typography.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { default as T } from 'material-ui/Typography';
+
+/**
+ *
+ * **Import:**
+ *
+ * `import Typography from 'es2k-react-components/style/Typography';`
+ *
+ * **Example:**
+ *
+ * `
+  <Typography type="display4" gutterBottom>
+    Display 4
+  </Typography>
+ * `
+ *
+ * **Todos:*
+ * - [ ] include another props
+ *
+ */
+const Typography = (props) => {
+  console.log('props', props);
+  return (
+    <T {...props}></T>;
+  );
+};
+
+Typography.propTypes = {
+  /**
+   * Typography theme
+   */
+  type: PropTypes.oneOf([
+    'display4', 'display3', 'display2', 'display1', 'headline',
+    'title', 'subheading', 'body2', 'body1', 'caption', 'button'
+  ]),
+};
+
+Typography.defaultProps = {
+  type: 'body1',
+};
+
+export default Typography;
